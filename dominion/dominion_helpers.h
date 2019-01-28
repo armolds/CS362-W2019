9c1dbc74d178a0901df6a4000464ccd885363575
davidwilson3 <wilsodav@flip1.engr.oregonstate.edu>
2019-01-27 19:05:04 -0800

    Finished assignment 2

--- dominion/dominion_helpers.h
+++ dominion/dominion_helpers.h
@@ -5,11 +5,21 @@
 
 int drawCard(int player, struct gameState *state);
 int updateCoins(int player, struct gameState *state, int bonus);
-int discardCard(int handPos, int currentPlayer, struct gameState *state, 
+int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
-int cardEffect(int card, int choice1, int choice2, int choice3, 
+int cardEffect(int card, int choice1, int choice2, int choice3,
 	       struct gameState *state, int handPos, int *bonus);
 
+int cardAdventurer(struct gameState *state);
+int cardCouncilRoom(struct gameState *state,  int handPos);
+int cardSmithy(struct gameState *state, int handPos);
+int cardVillage(struct gameState *state,  int handPos);
+int cardGreatHall(struct gameState *state,  int handPos);
+
+
+
 #endif
+
+
